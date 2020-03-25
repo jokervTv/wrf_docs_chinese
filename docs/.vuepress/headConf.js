1fc601bd1961c4efab975dfcf4725b358b054cb8
jokervTv <zhangyp6603@outlook.com>
2020-03-25 11:05:43 +0800

    添加百度统计和谷歌分析

--- docs/.vuepress/headConf.js
+++ docs/.vuepress/headConf.js
@@ -4,4 +4,18 @@ module.exports = [
     //['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }],
     //----- log && phone -----
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+    //----- baidu tongji -----
+    [
+        "script",
+        {},
+        `
+            var _hmt = _hmt || [];
+            (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?b0d4503fd4b0433e77fab0d0a924667b";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(hm, s);
+            })();
+        `
+    ]
 ]
--- docs/.vuepress/pluginConf.js
+++ docs/.vuepress/pluginConf.js
@@ -1,8 +1,17 @@
+const googleAnalytics = require('./googleAnalytics');
+
 module.exports = {
     'vuepress-plugin-smooth-scroll': {},
     'seo': {},
+    'sitemap': {
+        hostname: 'https://www.jokervtv.top/wrf/'
+    },
     'reading-progress': {},
     'vuepress-plugin-auto-sidebar': {
+    },
+    'vuepress-plugin-baidu-autopush': {},
+    '@vuepress/google-analytics': {
+        'ga': googleAnalytics
     },
     'flowchart':{}
 }
