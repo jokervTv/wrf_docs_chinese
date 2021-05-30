33c59c8632b665aafa3df6cb532da7be097f662e
Yongpeng Zhang <zhangyp6603@outlook.com>
2021-05-30 11:46:53 +0800

    update plugin

--- docs/.vuepress/headConf.js
+++ docs/.vuepress/headConf.js
@@ -1,21 +1,17 @@
 module.exports = [
     //----- latex -----
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.10.0-alpha/dist/katex.min.css' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.13.9/dist/katex.min.css' }],
     //['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }],
     //----- log && phone -----
+    ['link', { rel: 'icon', href: '/icons/icon-96x96.png' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-    //----- baidu tongji -----
-    [
-        "script",
-        {},
-        `
-            var _hmt = _hmt || [];
-            (function() {
-                var hm = document.createElement("script");
-                hm.src = "https://hm.baidu.com/hm.js?ec94b1d13ac80d83315288bd1c6130ef";
-                var s = document.getElementsByTagName("script")[0];
-                s.parentNode.insertBefore(hm, s);
-            })();
-        `
-    ]
+    //----- PWA ------
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
 ]
--- docs/.vuepress/pluginConf.js
+++ docs/.vuepress/pluginConf.js
@@ -2,6 +2,12 @@ const googleAnalytics = require('./googleAnalytics');
 
 module.exports = {
     'vuepress-plugin-smooth-scroll': {},
+    'vuepress-plugin-cat': {},
+    '@vuepress/last-updated': {
+        transformer(timestamp) {
+            return timestamp;
+        }
+    },
     'seo': {},
     'sitemap': {
         hostname: 'https://jokervtv.top/wrf/'
@@ -10,12 +16,42 @@ module.exports = {
     'vuepress-plugin-auto-sidebar': {
     },
     'vuepress-plugin-baidu-autopush': {},
-    '@vuepress/google-analytics': {
+    'minimal-analytics': {
         'ga': googleAnalytics
+    },
+    'flowchart': {},
+    'vuepress-plugin-code-copy': {
+        successText: "复制成功！",
+        backgroundColor: "#2FB800",
+        color: "#3eaf7c"
+    },
+    '@vuepress/pwa': {
+        serviceWorker: true,
+        updatePopup: {
+            message: "有新内容可用。",
+            buttonText: "刷新"
+        }
     },
     'graysite': {
         startDate: '2020-04-03 00:00:00',
         endDate: '2020-04-04 23:59:59'
+    }, 'vuepress-plugin-right-anchor': {
+        showLevel: 1,
     },
-    'flowchart':{}
+    //'vuepress-plugin-sponsor': {
+    //    theme: 'simple',
+    //    alipay: '/sponsor-qrcode/alipay.jpg',
+    //    wechat: '/sponsor-qrcode/wechat.jpg',
+    //    paypal: 'https://www.paypal.me/zhangyp6603',
+    //    duration: 2000
+    //},
+    'last-reading': {
+        popupConfig: {
+            message: '返回之前位置',
+            buttonText: '确定'
+        }
+    },
+    'check-md': {
+        pattern: '**/*.md'
+    },
 }
